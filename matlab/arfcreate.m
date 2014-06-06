f423863fcc7d8fccd869dd53d32de4f180652479
pmalonis <pmalonis@eggmix.uchicago.edu>
2014-06-06 16:01:35 -0500

    adding arfcreate.m

--- matlab/arfcreate.m
+++ matlab/arfcreate.m
@@ -23,18 +23,23 @@ function arfcreate(filename, datasetname, size, varargin)
         addParameter(p,'sampling_rate',0,@(x) (ceil(x)==x) & (x>0));
         addParameter(p,'arf_datatype',0,@(x) any(x==datatype_codes))
         parse(p,arfparams{:})
+        %converting p.Results into variables
+        for f = fieldnames(p.Results)' 
+            evalc([f{1} '= p.Results.(f{1})']); 
+        end
 
         %verify dataset attributes
-        if strcmp(p.Results.units,'')
-            if p.Results.sampling_rate == 0
+        if strcmp(units,'')
+            if sampling_rate == 0
                 err = MException('arf:InvalidAttribute',...
-                    ['Unitless data assumed time series and ' ... 
+                    ['Unitless data assumed time series and ' ...
                     'requires sampling_rate attribute.']);
                 throw(err)
             end
-        elseif strcmp(p.Results.units,'samples')
-            err = MExection('arf:InvalidAttribute',...
-                ['Data with units of "samples" requires ' ... 
+        elseif strcmp(units,'samples') & sampling_rate == 0
+            
+            err = MException('arf:InvalidAttribute',...
+                ['Data with units of "samples" requires ' ...
                 'sampling_rate attribute.']);
             throw(err)
         end
@@ -49,7 +54,7 @@ function arfcreate(filename, datasetname, size, varargin)
                 rethrow(err) 
             end
         end
-        new_file
+
         % obtaining list of new groups h5create will create
         new_groups = {}; 
         groups = strsplit(datasetname,'/');
@@ -59,7 +64,7 @@ function arfcreate(filename, datasetname, size, varargin)
             for i = 1:length(groups)
                 group_path = strjoin({groups{1:i}},'/');
                 try
-                    H5G.open(file_id, group_path);
+                    H5G.close(H5G.open(file_id, group_path));
                 catch err
                     if strcmp(err.identifier,'MATLAB:imagesci:hdf5lib:libraryError')
                         new_groups = [new_groups, group_path];
@@ -88,25 +93,26 @@ function arfcreate(filename, datasetname, size, varargin)
         %add timestamp and uuid to group attributes
         for i = 1:length(new_groups)
             [~,uuid] = system('uuidgen');
-            if p.Results.timestamp == 0
+            if timestamp == 0
                 [~,tstamp] = system('date +%s');
+                tstamp = uint64([str2double(tstamp) 0]);
             else
-                tstamp = p.Results.timestamp;
+                tstamp = uint64(timestamp);
             end
-            h5writeatt(filename,new_groups{i},'uuidgen',uuid)
+            h5writeatt(filename,new_groups{i},'uuid',uuid)
             h5writeatt(filename,new_groups{i},'timestamp',tstamp)
         end
         
         %add attributes to dataset
-        if ~strcmp(p.Results.units,'')
-            h5writeatt(filename,datasetname,'units',p.Results.units)
+        if ~strcmp(units,'')
+            h5writeatt(filename,datasetname,'units',units)
         end
-        if p.Results.sampling_rate > 0
+        if sampling_rate > 0
             h5writeatt(filename,datasetname,'sampling_rate',...
-                p.Results.sampling_rate)
+                sampling_rate)
         end 
         h5writeatt(filename,datasetname,'datatype',...
-            uint16(p.Results.arf_datatype))
+            uint16(arf_datatype))
         
         
 end
