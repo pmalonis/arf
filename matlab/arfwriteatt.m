da6aedeace364b7f210314520615825528f683d0
pmalonis <pmalonis@eggmix.uchicago.edu>
2014-06-10 10:03:05 -0500

    fixed arfwriteatt.m, same error as before

--- matlab/arfwriteatt.m
+++ matlab/arfwriteatt.m
@@ -7,11 +7,11 @@ function arfwriteatt(filename,location,attname,attvalue)
         else
             type_id = H5T.copy('H5T_NATIVE_DOUBLE');
         end
-        fid = H5F.open(filename);
+        fid = H5F.open(filename,'H5F_ACC_RDWR','H5P_DEFAULT');
         acpl_id = H5P.create('H5P_ATTRIBUTE_CREATE');
-        loc_id = HFO.open(location,'H5P_DEFAULT');
+        loc_id = H5O.open(fid,location,'H5P_DEFAULT');
         space_id = H5S.create('H5S_SCALAR');
-        attr_id = H5A.create(loc_id,'',type_id,space_id,acpl_id);
+        attr_id = H5A.create(loc_id,attname,type_id,space_id,acpl_id);
         H5A.write(attr_id,'H5ML_DEFAULT',attvalue)
     end
 end
